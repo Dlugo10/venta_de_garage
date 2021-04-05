3c6b4e26ed4e2c0bdd4893dae35631189844f3d9
david lugo <davidlugo936@gmail.com>
2021-04-05 11:50:33 -0500

    Added Cloudinary.rb config file

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = '@dltobqqbw'
+  config.api_key = '712166111673163'
+  config.api_secret = 'xbCTDO2hgK0rD3IxlTT9dus3Gic'
+  config.secure = true
+  config.cdn_subdomain = true
+end
